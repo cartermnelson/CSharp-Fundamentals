78a2cd8eebc40864561d3f664983bbbbcc641434
Carter Nelson <carternelson05@gmail.com>
2019-09-18 15:04:51 -0700

    Added while loops

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace Fundamentals1
     {
         static void Main(string[] args)
         {
-            // // prints all values from 1-255
+            // prints all values from 1-255
             for (int i = 1; i <= 255; i++)
             {
                 Console.WriteLine(i);
             }
 
-            // // prints all values from 1-100 that are divisible by 3 or 5, but not both
+            // prints all values from 1-100 that are divisible by 3 or 5, but not both
             for (int i = 1; i <= 100; i++)
             {
                 if ((i % 3 == 0 || i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0))
@@ -38,6 +38,46 @@ namespace Fundamentals1
                 }
             }
 
+
+            // Rewrite all for loops as while loops
+
+
+            int j = 1;
+            // prints all values from 1-255
+            while (j <= 255)
+            {
+                Console.WriteLine(j++);
+            }
+
+            // prints all values from 1-100 that are divisible by 3 or 5, but not both
+            j = 1;
+            while (j <= 100)
+            {
+                if ((j % 3 == 0 || j % 5 == 0) && !(j % 3 == 0 && j % 5 == 0))
+                {
+                    Console.WriteLine(j);
+                }
+                j++;
+            }
+
+            // print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+            j = 1;
+            while (j <= 100)
+            {
+                if (j % 3 == 0 && j % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (j % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (j % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                j++;
+            }
         }
     }
 }
